686eb9447f9ae6dbff3b91cebff913fa147eb3a6
hoangnh92 <hoangnh.coder@gmail.com>
2023-05-05 10:13:50 +0700

    config k8s

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad9698b842284465da3b79854d4a5202-1785831754.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
